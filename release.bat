9a3b3d2eda98775d1af689211fb3c719719dd4bb
darkconsole <darkcee.legit@gmail.com>
2019-09-07 18:54:03 -0500

    script to build release zips

---
+++ release.bat
@@ -0,0 +1,6 @@
+@echo off
+
+echo DID YOU INCREMEMENT THE VERSION NUMBER IN THE MAIN SCRIPT ASSHOLE?
+
+c:\local\unxutils\usr\local\wbin\zip dse-stocking-outfit-v%~1.zip -q -r README.md *.esp CalienteTools meshes SKSE textures
+
