7ac6b0dc5646c05b7b440efa60eff0c4322373e2
darkconsole <darkcee.legit@gmail.com>
2019-09-17 20:43:02 -0500

    include the fkn scripts directory omg

--- release.bat
+++ release.bat
@@ -2,5 +2,5 @@
 
 echo DID YOU INCREMEMENT THE VERSION NUMBER IN THE MAIN SCRIPT ASSHOLE?
 
-c:\local\unxutils\usr\local\wbin\zip dse-stocking-outfit-v%~1.zip -q -r README.md *.esp CalienteTools meshes SKSE textures
+c:\local\unxutils\usr\local\wbin\zip dse-stocking-outfit-v%~1.zip -q -r README.md *.esp CalienteTools meshes scripts SKSE textures
 
